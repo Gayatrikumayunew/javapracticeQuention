2546dcd36d235ad02ceddfe5c06c572812e9e389
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2023-12-31 23:53:32 +0530

    Add files via upload

---
+++ _30factorial.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gayatri
+ */
+public class _30factorial {
+    public static void main(String[] ar){
+            int n=5;
+            int ans=1;
+for(int i=2; i<=n ;i++){
+            ans *=i ;
+}
+System.out.println(ans);
+    }
+    
+}
+
