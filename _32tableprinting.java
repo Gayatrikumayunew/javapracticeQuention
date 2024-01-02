d3604c3e7c72fd1ce853fddbe1dbd98e26459f62
gayatri_kumayu <126497909+Gayatrikumayunew@users.noreply.github.com>
2024-01-02 15:20:16 +0530

    Add files via upload

---
+++ _32tableprinting.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author gayatri
+ */
+public class _32tableprinting {
+    public static void main(String []ar){
+            int num=6;
+            int ans=0;
+            for(int i=1;i<11;i++){
+                ans=num*i;
+            System.out.println(num + " * " + i + " = " + ans);
+                
+            }
+    }
+    
+}
